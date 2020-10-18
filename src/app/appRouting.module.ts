af14eefb0c290baeaededb1c880de248c2c46c63
Hunter-777 <59577881+Hunter-777@users.noreply.github.com>
2020-10-18 21:35:10 +0530

    By Webstorm

---
+++ src/app/appRouting.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {AddUserComponent} from "./add-user/add-user.component";
+
+
+const routes: Routes = [
+    {path: 'addUser', component: AddUserComponent}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
+export const routingComponents = [AddUserComponent];
