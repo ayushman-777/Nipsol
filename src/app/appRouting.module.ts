8694f1a7894f1b5a352342d20a295b7651dd86e1
Hunter-777 <pathakayush123@gmail.com>
2020-10-19 13:20:17 +0530

    2 files modified

--- src/app/appRouting.module.ts
+++ src/app/appRouting.module.ts
@@ -8,6 +8,7 @@ import {HomeComponent} from "./home/home.component";
 
 const routes: Routes = [
     {path: 'addUser', component: AddUserComponent},
+    {path: '', component: HomeComponent},
     {path: 'home', component: HomeComponent}
 ];
 
