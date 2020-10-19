30a09de94a6605ada4f51c9905d797659717c3b9
Hunter-777 <pathakayush123@gmail.com>
2020-10-19 18:00:33 +0530

    2 files modified

--- src/app/add-user/add-user.component.ts
+++ src/app/add-user/add-user.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-add-user',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-user.component.css']
 })
 export class AddUserComponent implements OnInit {
-  public currUserNameUser = '';
+  currUserNameUser = '';
   constructor() {
   }
 
