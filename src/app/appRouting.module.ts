4a5c73cd6037b5936f77a076e6efb5841fcacba2
Hunter-777 <59577881+Hunter-777@users.noreply.github.com>
2020-10-20 15:25:49 +0530

    By Webstorm

--- src/app/appRouting.module.ts
+++ src/app/appRouting.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddUserComponent } from './add-user/add-user.component';
 import {HomeComponent} from "./home/home.component";
+import {AllUserComponent} from "./all-user/all-user.component";
 
 
 
@@ -9,7 +10,8 @@ import {HomeComponent} from "./home/home.component";
 const routes: Routes = [
     {path: 'addUser', component: AddUserComponent},
     {path: '', component: HomeComponent},
-    {path: 'home', component: HomeComponent}
+    {path: 'home', component: HomeComponent},
+    {path: 'allUser', component: AllUserComponent}
 ];
 
 @NgModule({
@@ -17,4 +19,4 @@ const routes: Routes = [
     exports: [RouterModule]
 })
 export class AppRoutingModule {}
-export const routingComponents = [AddUserComponent, HomeComponent];
+export const routingComponents = [AddUserComponent, HomeComponent, AllUserComponent];
