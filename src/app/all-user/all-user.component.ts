0a37f6845f825559ec4928baf6d1d525bd6a1447
Hunter-777 <59577881+Hunter-777@users.noreply.github.com>
2020-10-22 16:01:03 +0530

    By Webstorm

--- src/app/all-user/all-user.component.ts
+++ src/app/all-user/all-user.component.ts
@@ -8,15 +8,18 @@ import {ActivatedRoute, Router} from "@angular/router";
   styleUrls: ['./all-user.component.css']
 })
 export class AllUserComponent implements OnInit {
-
+  userDetails:any[];
   constructor(private router: Router,
               private route: ActivatedRoute) {
   }
 
   ngOnInit() {
     this.route.queryParams.subscribe((params)=>{
-      console.log(params)
+      console.log(params);
+      this.userDetails.push(JSON.parse(params.userDetail));
     })
   }
-
+  check(){
+    console.log(this.userDetails[0].email);
+  }
 }
